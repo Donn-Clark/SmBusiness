a2433cee98b3f37a44ec131b88a799cee78423d8
DonnClark <donnclark@sbcglobal.net>
2014-06-03 20:29:57 -0700

    Initial setup on GitHub
    Small Business Landing website -- Thinkful

---
+++ js/_vti_cnf/lightbox.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|DEC_VAIO\\Donn
+vti_modifiedby:SR|DEC_VAIO\\Donn
+vti_timelastmodified:TR|29 Apr 2014 18:08:57 -0000
+vti_timecreated:TR|29 Apr 2014 18:08:25 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|about.html
+vti_cacheddtm:TX|29 Apr 2014 18:08:57 -0000
+vti_filesize:IR|20067
+vti_nexttolasttimemodified:TR|29 Apr 2014 18:08:25 -0000
---
+++ js/_vti_cnf/slider.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|29 Apr 2014 13:40:59 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|DEC_VAIO\\Donn
+vti_modifiedby:SR|DEC_VAIO\\Donn
+vti_timecreated:TR|26 Apr 2014 11:39:49 -0000
+vti_backlinkinfo:VX|contact.html products.html services.html slider.dwt team.html about.html main.html index.html
+vti_cacheddtm:TX|29 Apr 2014 13:40:59 -0000
+vti_filesize:IR|4052
+vti_nexttolasttimemodified:TR|26 Apr 2014 11:39:49 -0000
